5902f1d8a9a3b7539182c6d0a40ea317856db77f
Haios-Paradox <haiosvida@gmail.com>
2022-03-25 16:07:16 +0700

    Working on the local database, still need to add the foreign keys tho...

--- app/src/main/java/com/example/livetest/core/database/Entities.kt
+++ app/src/main/java/com/example/livetest/core/database/Entities.kt
@@ -3,39 +3,51 @@ package com.example.livetest.core.database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "idol_data")
+
+@Entity(tableName = "idol_data")//This data is pre-populated
 data class Idols(
     @PrimaryKey(autoGenerate = false)
-    val idol_id : String
+    val idol_id : String,
+    val channel_id : String,//TODO: This is a foreign key
+    val idol_name : String,
 )
 
 @Entity(tableName = "channel_data")
 data class Channels(
-    val channel_id : String
+    @PrimaryKey(autoGenerate = false)
+    val channel_id : String,
+    val channel_avatar : String,
+    val channel_type : String,
+    var channel_follow : Boolean = false
 )
 
 @Entity(tableName = "video_data")
 data class Videos(
-    val video_id : String
+    @PrimaryKey(autoGenerate = true)
+    val video_id : String,
+    val video_url : String,
+    var video_fav : Boolean = false
 )
 
-@Entity(tableName = "video_playlist")//TODO: Insert Foreign Key
+@Entity(tableName = "video_playlist")
 data class video_playlist(
-    val video_id: String,
-    val playlist_id: String
+    @PrimaryKey(autoGenerate = false)
+    val list_id: String,
+    val video_fk: String,
+    val playlist_fk: String
 )
 
 @Entity(tableName = "playlist_data")
 data class Playlist(
-    val playlist_id : String
+    @PrimaryKey(autoGenerate = false)
+    val playlist_id : String,
+    val playlist_name : String,
+    val playlist_type : String
 )
 
 @Entity(tableName = "oshi_data")
-data class Oshis(
-    val oshi_id : String
-)
-
-@Entity(tableName = "content")
-data class Contents(
-    val content_id : String
+data class Oshi(
+    val oshi_id : String,
+    val idol_fk : String,
+    val oshi_nickname : String
 )
